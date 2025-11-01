667fc645d40da38db69dacdf43d41598f884ccec
Fran0808 <fortegaquipse@gmail.com>
2025-11-01 14:32:07 -0500

    Añadiendo ruta a categoria

---
+++ backend/routes/categoriaRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrl = require('../controllers/categoriaController');
+
+//Definir las rutas
+router.get('/', ctrl.obtenerCategorias);
+router.get('/:id', ctrl.obtenerCategoriaPorId);
+router.post('/', ctrl.crearCategoria);
+router.put('/:id', ctrl.actualizarCategoria);
+router.delete('/:id', ctrl.eliminarCategoria);
+
+module.exports = router;
