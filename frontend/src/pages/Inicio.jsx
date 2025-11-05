583826bb714d008c7af18895762e52d6ce356787
Fran0808 <fortegaquipse@gmail.com>
2025-11-05 12:01:27 -0500

    Modificando el index

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,7 +1,9 @@
+// Inicio.jsx
 import React, { useEffect, useState } from "react";
 import TablasView from "../componentes/Tablas/TablasView";
 import "./Style.css";
-import Cards from "../componentes/Cards/Cards"; // Ajusta la ruta según tu proyecto
+import Cards from "../componentes/Cards/Cards";
+import GraficoMovimiento from "../componentes/Graficos/GraficoMovimiento";
 
 function Inicio() {
   const [resumen, setResumen] = useState({
@@ -56,8 +58,9 @@ function Inicio() {
   return (
     <div className="mx-4">
       <h1>PANEL DE INVENTARIO</h1>
+
       {/* Resumen con KPIs */}
-      <div className="row mb-1">
+      <div className="row mb-3">
         <div className="col-md-3 mb-3">
           <Cards titulo="Total Productos" valor={resumen.total_productos} />
         </div>
@@ -78,10 +81,16 @@ function Inicio() {
         </div>
       </div>
 
+      {/* Gráfico */}
+      <div className="mb-4">
+        <GraficoMovimiento />
+      </div>
+
       {/* Tablas */}
       <h2>Productos con stock bajo</h2>
       <TablasView data={stockBajo} />
-      <h2 className="mt-1">Movimientos recientes</h2>
+
+      <h2 className="mt-4">Movimientos recientes</h2>
       <TablasView data={movimientosRecientes} />
     </div>
   );
